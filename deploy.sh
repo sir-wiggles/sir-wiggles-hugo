e04028f89fe8c3086f0cc91a0bfc7775df3dd869
Jeff <jgregory@bitcasa.com>
2016-01-07 16:10:44 -0800

    rebuilding site Thu Jan  7 16:10:42 PST 2016

--- deploy.sh
+++ deploy.sh
@@ -7,14 +7,16 @@ if [ $# -eq 1 ]
   then msg="$1"
 fi
 
-# Build the project.
+echo -e "\033[0;33mBuilding pages...\033[0m"
 hugo -d sir-wiggles.github.io
 
+echo -e "\033[0;33mPushing public...\033[0m"
 cd sir-wiggles.github.io
 git add -A
 git commit -m "$msg"
 git push origin master
 
+echo -e "\033[0;33mPushing source...\033[0m"
 cd ..
 git add -A
 git commit -m "$msg"
